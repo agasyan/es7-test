ef6961a2d9906c80c10b48b34f9518a47fd91150
Agas Yanpratama <agas.yanpratama@gmail.com>
2024-03-31 15:06:09 +0700

    add index only

--- scripts/ltindexer/ltindexer.go
+++ scripts/ltindexer/ltindexer.go
@@ -12,17 +12,19 @@ import (
 
 // Define a struct that matches the JSON structure
 type myData struct {
-	Count int `json:"count"`
+	Count   int  `json:"count"`
+	IsIndex bool `json:"is_index"`
 }
 
-func NewCustomTargeter() vegeta.Targeter {
+func NewCustomTargeter(IsIndexOnly bool) vegeta.Targeter {
 	return func(tgt *vegeta.Target) error {
 		if tgt == nil {
 			return vegeta.ErrNilTarget
 		}
 
 		data := myData{
-			Count: gofakeit.Number(1, 5),
+			Count:   gofakeit.Number(1, 5),
+			IsIndex: IsIndexOnly,
 		}
 		jsonData, _ := json.Marshal(data)
 
@@ -37,8 +39,9 @@ func NewCustomTargeter() vegeta.Targeter {
 func main() {
 	// lt 40 bau 20
 	rate := vegeta.Rate{Freq: 40, Per: 1 * time.Second}
+	IsIndexOnly := true
 	duration := 600 * time.Second
-	targeter := NewCustomTargeter()
+	targeter := NewCustomTargeter(IsIndexOnly)
 	attacker := vegeta.NewAttacker()
 	var metrics vegeta.Metrics
 	for res := range attacker.Attack(targeter, rate, duration, "Load Test") {
